f5acd3c5457d98fd54d1d8fa29cd67517c0e281f
Pavle Šarenac <sarenac.pavle@gmail.com>
2023-04-25 12:18:49 +0200

    Automatski formatiran kod u svim fajlovima

--- TimMajstorNaKlik (nakon FR)/prototip/script.js
+++ TimMajstorNaKlik (nakon FR)/prototip/script.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#logo").on({
-        mouseenter : function() {
+        mouseenter: function () {
             $(this).css("border-color", "yellow");
         },
 
-        mouseleave : function() {
+        mouseleave: function () {
             $(this).css("border-color", "black");
         }
     });
 
     $("button").on({
-        mouseenter : function() {
+        mouseenter: function () {
             $(this).css("border", "4px solid yellow");
         },
 
-        mouseleave : function() {
+        mouseleave: function () {
             $(this).css("border", "none");
         }
     });
 
     $("a, h1").on({
-        mouseenter : function() {
+        mouseenter: function () {
             $(this).css("font-weight", "bold");
         },
 
-        mouseleave : function() {
+        mouseleave: function () {
             $(this).css("font-weight", "normal");
         }
     });
